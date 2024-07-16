188bbab93c6e687e3645ba007da5a3e90a871a4d
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2024-07-16 11:24:58 +0600

    feat: Add lazy loading for Hero component image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,15 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Hero = () => {
   return (
     <div>
-        <div className='container mx-auto grid grid-cols-12 gap-2 '>
-            <div className='col-span-3 border border-primary '>
+        <div className='container mx-auto grid grid-cols-12 gap-2 py-2 '>
+            <div className='col-span-12 md:col-span-3 '>
                 menu
             </div>
-            <div className='col-span-9 border border-primary'>
-                silder
+            <div className='col-span-12 md:col-span-9 rounded-lg'>
+                <Image src="/hero-bg.jpg" alt="banner" width={1000} height={500}/>
             </div>
         </div>
     </div>
